c3cb64915c8d04491fdcff556e31a4fd604b0064
thanhb2000148 <thanhb2000148@student.ctu.edu.vn>
2023-10-06 07:53:38 +0700

    giai bai tap

---
+++ app/controllers/controller.js
@@ -0,0 +1,27 @@
+export.create = (req, res) => {
+    res.send({ message: "create handler"});
+};
+
+export.findALL = (req, res) => {
+    res.send({ message: "findALL handler"});
+};
+
+export.findOne = (req, res) => {
+    res.send({ message: "update handler"});
+};
+
+export.update = (req, res) => {
+    res.send({ message: "update handler"});
+};
+
+export.delete = (req, res) => {
+    res.send({ message: "delete handler"})''
+};
+
+export.deleteALL = (req, res) => {
+    res.send({ message; "deleteALL handler"});
+};
+
+export.findALLFavorite = (req, res) => {
+    res.send({ message: "findALLFavorite handler"});
+};
---
+++ app/routes/contact.route.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const contact = require("../controller/contact.controller");
+
+const router = express.Router();
+
+router
+  .route("/")
+  .get(contact.findALL)
+  .post(contact.create)
+  .delete(contact.deleteALL);
+
+router.route("/favorite").get(contact.findALLFavorite);
+
+router
+  .route("/:id")
+  .get(contact.findOne)
+  .put(contact.update)
+  .delete(contact.delete);
+
+module.exports = router;
