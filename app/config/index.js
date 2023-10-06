038312acb82cd6c17dea5d5a37e445b22710249d
thanhb2000148 <thanhb2000148@student.ctu.edu.vn>
2023-10-06 11:48:35 +0700

    cai dat cac handler truy xuat csdl

--- app/config/index.js
+++ app/config/index.js
@@ -2,6 +2,9 @@ const config = {
   app: {
     port: process.env.port || 3000,
   },
+  db: {
+    uri: process.env.MONGODB_URI || "mongodb://localhost:27017"
+  }
 };
 
 module.exports = config;
--- server.js
+++ server.js
@@ -1,8 +1,17 @@
-constapp = require("./app");
-const config = require("./app/config");
-
-// start sever
-const PORT = config.app.port;
-app.isten(PORT, () => {
-  console.log("server is running on port ${PORT}.");
-});
+const app = require("./app");
+const config = require ("./app/config");
+const MongoDB = require("./app/utils/mongobd.util");
+async function startServer(){
+  try{
+    await MongoDB.connect(config.db.uri);
+    console.log("Connected to the database!");
+    const PORT = config.app.port;
+    app.listen(PORT, ()=>{
+      console.log("Server is running on port ${PORT}");
+    });
+  }catch(error){
+    console.log("Cannot connect to the database!",error);
+    process.exit();
+  }
+}
+startServer();
